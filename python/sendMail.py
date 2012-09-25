01be9a2b2fa459661f3aaabc072addfcce241bb4
Max Lin <max.lin@gmx.com>
2012-09-25 19:00:46 +0800

    add some files

---
+++ python/sendMail.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import smtplib
+import getopt
+import sys
+import string
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "", ["sender=", "receiver="])
+except getopt.error, msg:
+    print 'python sendMail.py --sender [sender address] --receiver [receiver address] '
+    sys.exit(2)
+
+# process sender and receiver
+sender = ''
+receiver = ''
+
+for o, a in opts:
+    if o == "--sender":
+        sender = a
+    elif o == "--receiver":
+        receiver = a
+
+if sender == '' or receiver == '':
+    print 'python sendMail.py --sender [sender address] --receiver [receiver address] '
+    sys.exit(2)
+
+# scheduled the content
+sender_str = 'From:'
+receiver_str = 'To:'
+
+sender_str += sender
+receiver_str += receiver
+
+message = sender_str + '\n' + receiver_str + '\n'
+message += """Subject: subscribe request
+
+I wanna subscribe this ML.
+"""
+
+# managed to send mail
+try:
+  smtpObj = smtplib.SMTP('localhost')
+  smtpObj.sendmail(sender, receiver, message)
+  print "Successfully sent email"
+except SMTPException:
+  print "Error: unable to send email"
